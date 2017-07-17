680471d3597387da2bcca0dc09135ded4e5233f9
Joelle Hinton <jhinton4@sci227lab14.spelman.edu>
2017-07-17 14:18:42 -0400

    Fixed login page

--- curlyworld/main.py
+++ curlyworld/main.py
@@ -25,10 +25,42 @@ jinja_environment = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        #user = users.get_current_user()
+        #if user:
+            #nickname = user.nickname()
+            #logout_url = users.create_logout_url('/')
+            #greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                #nickname, logout_url)
+        #else:
+            #login_url = users.create_login_url('/')
+            #greeting = '<a href="{}">Sign in</a>'.format(login_url)
+
+        #self.response.write(
+            #'<html><body>{}</body></html>'.format(greeting))
+        user = 'Chloe'
+        email = ''
+        email2 = ''
+        password = ''
+        password2 = ''
+        template_values = {
+            'uname' : user,
+            'email': email,
+            'email2': email2,
+            'psw': password,
+            'psw2': password2,
+        }
         template = jinja_environment.get_template('template/mainpage.html')
-        self.response.out.write(template.render())
+        self.response.out.write(template.render(template_values))
+
+
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        print('Hello World')
+
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/Login', Guestbook),
 ], debug=True)
